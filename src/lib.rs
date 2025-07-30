c6dbcae95cd85f45ec7fa2777d8bb6fee9b8ab72
Daniel Levin <daniellevin2607@gmail.com>
2025-07-30 08:08:18 +0200

    basic topdown  - definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+use perf_event::events::Hardware;
+use perf_event::{Builder, Group};
 
+pub fn topdown<T, F: Fn() -> T>(f: F) -> std::io::Result<T> {
+    let mut group = Group::new()?;
+    let cycles = group.add(&Builder::new(Hardware::CPU_CYCLES))?;
+
+    group.enable()?;
+    let res = f();
+    group.disable()?;
+
+    let counts = group.read()?;
+
+    dbg!(counts);
+
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a() {
+        let res = topdown(|| {
+        }).unwrap();
+    }
+}
