bfd32fe52844e5b19b9894baeb3c1d3986738c21
Daniel Levin <daniellevin2607@gmail.com>
2025-07-30 08:11:27 +0200

    measurement

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use perf_event::{Builder, Group};
 
 pub fn topdown<T, F: Fn() -> T>(f: F) -> std::io::Result<T> {
     let mut group = Group::new()?;
-    let cycles = group.add(&Builder::new(Hardware::CPU_CYCLES))?;
+    let cycles = group.add(&Builder::new(Hardware::BRANCH_MISSES))?;
 
     group.enable()?;
     let res = f();
@@ -11,7 +11,9 @@ pub fn topdown<T, F: Fn() -> T>(f: F) -> std::io::Result<T> {
 
     let counts = group.read()?;
 
-    dbg!(counts);
+    let bm = counts[&cycles];
+
+    dbg!(bm);
 
     Ok(res)
 }
@@ -23,6 +25,19 @@ mod tests {
     #[test]
     fn a() {
         let res = topdown(|| {
+            let mut sum = 0u64;
+            for i in 0..10000 {
+                if i % 3 == 0 {
+                    sum = sum.wrapping_add(i * 17);
+                } else if i % 5 == 0 {
+                    sum = sum.wrapping_sub(i / 2);
+                } else if i % 7 == 0 {
+                    sum = sum.wrapping_mul(2);
+                } else {
+                    sum = sum.wrapping_add(i.wrapping_pow(2) % 1000);
+                }
+            }
+            sum
         }).unwrap();
     }
 }
