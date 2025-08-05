7d0c87ed3395a18d63a861149ea5820bbd9bec20
Daniel Levin <daniellevin2607@gmail.com>
2025-08-05 23:09:29 +0200

    basic scope derivation

--- dirty_mike_derive/src/lib.rs
+++ dirty_mike_derive/src/lib.rs
@@ -1,1 +1,19 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
 
+#[proc_macro_derive(Scope)]
+pub fn derive_scope(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+
+    let expanded = quote! {
+        impl Scope for #name {
+            fn scope(&self) -> &'static str {
+                stringify!(#name)
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
