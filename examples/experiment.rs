79669c4120c4f2fca222a1a0fde29502269ffc9c
Daniel Levin <daniellevin2607@gmail.com>
2025-08-07 11:34:56 +0200

    specific bug

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -1,5 +1,4 @@
 use perf_event::{Builder, events};
-use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event = Builder::new(events::Software::CONTEXT_SWITCHES)
@@ -12,14 +11,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for i in 0..10 {
         println!("Waiting for event {}...", i);
-        match sampled.next_blocking(Some(Duration::from_millis(500))) {
+        match sampled.next_blocking(None) {
             Some(sample) => {
-                println!(
-                    "Got event {}! of type {} with size {}",
-                    i,
-                    sample.ty(),
-                    sample.len()
-                );
                 match sample.parse_record() {
                     Ok(record) => println!("Event record: {:?}", record),
                     Err(e) => eprintln!("Failed to parse record: {}", e),
