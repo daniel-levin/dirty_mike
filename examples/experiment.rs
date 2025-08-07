5cff82f3dcabba4dfe0d20c1a632e7f61b2b42bf
Daniel Levin <daniellevin2607@gmail.com>
2025-08-07 11:46:56 +0200

    disappoint

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -1,23 +1,21 @@
-use perf_event::{Builder, events};
+use perf_event::{Builder, Group, events};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let event = Builder::new(events::Software::CONTEXT_SWITCHES)
-        .context_switch(true)
-        .build()?;
+    let group = Group::builder().context_switch(true).build_group()?;
 
-    let mut sampled = event.sampled(128)?;
+    let ctr = group.into_counter();
+
+    let mut sampled = ctr.sampled(128)?;
 
     sampled.enable()?;
 
     for i in 0..10 {
         println!("Waiting for event {}...", i);
-        match sampled.next_blocking(None) {
-            Some(sample) => {
-                match sample.parse_record() {
-                    Ok(record) => println!("Event record: {:?}", record),
-                    Err(e) => eprintln!("Failed to parse record: {}", e),
-                }
-            }
+        match sampled.next_blocking(Some(std::time::Duration::from_millis(500))) {
+            Some(sample) => match sample.parse_record() {
+                Ok(record) => println!("Event record: {:?}", record),
+                Err(e) => eprintln!("Failed to parse record: {}", e),
+            },
             None => {
                 eprintln!("timeout");
             }
