6ab6d93318ebfcff2632af275a52deeb983764af
Daniel Levin <daniellevin2607@gmail.com>
2025-08-07 16:39:17 +0200

    worse

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -5,17 +5,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tp = events::Hardware::CPU_CYCLES;
 
     let mut sampler = Builder::new(tp)
-        .sample_period(1000)
+        .sample_period(1_000)
         .build()?
         .sampled(8192)?;
 
     sampler.enable()?;
 
-    for _i in 0..100 {
-        match sampler.next_blocking(Some(std::time::Duration::from_millis(1200))) {
+    for i in 0..100 {
+        match sampler.next_blocking(None) {
             Some(sample) => match sample.parse_record() {
                 Ok(record) => {
-                    println!("Event record: {:#?}", &record);
+                    println!("Event record {i}: {:#?}", &record);
                 }
                 Err(e) => eprintln!("Failed to parse record: {}", e),
             },
