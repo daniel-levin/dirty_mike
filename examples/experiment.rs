4ee00e3538efab9cd10a6b747c1f9ce79a66f623
Daniel Levin <daniellevin2607@gmail.com>
2025-08-07 12:08:19 +0200

    got type 14

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -12,10 +12,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 0..10 {
         println!("Waiting for event {}...", i);
         match sampled.next_blocking(Some(std::time::Duration::from_millis(500))) {
-            Some(sample) => match sample.parse_record() {
-                Ok(record) => println!("Event record: {:?}", record),
-                Err(e) => eprintln!("Failed to parse record: {}", e),
-            },
+            Some(sample) => {
+                println!("got sample of type {}", sample.ty());
+
+                match sample.parse_record() {
+                    Ok(record) => println!("Event record: {:?}", record),
+                    Err(e) => eprintln!("Failed to parse record: {}", e),
+                }
+            }
             None => {
                 eprintln!("timeout");
             }
