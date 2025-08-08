4814cb81d323da9414c807c386c2015c8c5b01af
Daniel Levin <daniellevin2607@gmail.com>
2025-08-08 13:17:24 +0200

    good

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -1,14 +1,9 @@
-use perf_event::{Builder, ReadFormat, SampleFlag, events};
+use perf_event::{Builder, ReadFormat, SampleFlag, events, events::Tracepoint};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tp = events::Tracepoint::with_name("block/block_rq_complete")?;
-    //let tp = events::Hardware::CPU_CYCLES;
-
-    let mut sampler = Builder::new(tp)
+    let mut sampler = Builder::new(Tracepoint::with_name("block/block_rq_complete")?)
         .any_pid()
         .one_cpu(1)
-        .include_kernel()
-        .sample(SampleFlag::RAW)
         .sample_period(1)
         .build()?
         .sampled(8192)?;
