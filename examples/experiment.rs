b4ff29ebc5a3e284e7a124d79a859dc5ab7c5451
Daniel Levin <daniellevin2607@gmail.com>
2025-08-08 11:09:27 +0200

    oh no

--- examples/experiment.rs
+++ examples/experiment.rs
@@ -1,10 +1,21 @@
-use perf_event::{Builder, events};
+use perf_event::{Builder, SampleFlag, events};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    /*let tp = events::Tracepoint::with_name("block/block_rq_complete")?;*/
-    let tp = events::Hardware::CPU_CYCLES;
+    let tp = events::Tracepoint::with_name("block/block_rq_complete")?;
+    //let tp = events::Hardware::CPU_CYCLES;
 
     let mut sampler = Builder::new(tp)
+        //.include_kernel()
+        .sample(
+            SampleFlag::RAW
+                | SampleFlag::IP
+                | SampleFlag::TID
+                | SampleFlag::TIME
+                | SampleFlag::CPU
+                | SampleFlag::PERIOD
+                | SampleFlag::RAW
+                | SampleFlag::IDENTIFIER,
+        )
         .sample_period(1_000)
         .build()?
         .sampled(8192)?;
@@ -12,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     sampler.enable()?;
 
     for i in 0..100 {
-        match sampler.next_blocking(None) {
+        match sampler.next_blocking(Some(std::time::Duration::from_millis(500))) {
             Some(sample) => match sample.parse_record() {
                 Ok(record) => {
                     println!("Event record {i}: {:#?}", &record);
