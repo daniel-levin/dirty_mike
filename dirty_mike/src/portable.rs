bcab673e04f1a53d6aa8c64476e462a39424f6c6
Daniel Levin <daniellevin2607@gmail.com>
2025-08-12 16:51:50 +0200

    More counters

--- dirty_mike/src/portable.rs
+++ dirty_mike/src/portable.rs
@@ -8,14 +8,17 @@ pub struct BasicHardwareCounters {
     #[hardware(INSTRUCTIONS)]
     pub instructions: u64,
 
-    #[hardware(BRANCH_MISSES)]
-    pub branch_misses: u64,
+    #[hardware(CACHE_MISSES)]
+    pub cache_misses: u64,
 
     #[hardware(BRANCH_INSTRUCTIONS)]
     pub branch_instructions: u64,
 
-    #[hardware(CACHE_MISSES)]
-    pub cache_misses: u64,
+    #[hardware(BRANCH_MISSES)]
+    pub branch_misses: u64,
+
+    #[hardware(REF_CPU_CYCLES)]
+    pub ref_cpu_cycles: u64,
 }
 
 #[cfg(test)]
@@ -46,6 +49,8 @@ mod tests {
         let (_, sorted_res) = BasicHardwareCounters::measure(|| work(&sorted_data)).unwrap();
 
         let (_, unsorted_res) = BasicHardwareCounters::measure(|| work(&shuffled_data)).unwrap();
+
+        dbg!(&sorted_res);
 
         assert!(sorted_res.branch_misses * 100 < unsorted_res.branch_misses);
     }
