4f9a2035141742c423a1b2eeb4e3892ec5c10080
Daniel Levin <daniellevin2607@gmail.com>
2025-08-27 15:51:32 +0200

    additional perfmon counters

--- util/perfmon2rust/src/main.rs
+++ util/perfmon2rust/src/main.rs
@@ -1,5 +1,15 @@
 use serde::{Deserialize, Serialize};
 
+fn dehex<'de, D: serde::Deserializer<'de>>(d: D) -> Result<u8, D::Error> {
+    let s = <&str>::deserialize(d)?;
+    Ok(u8::from_str_radix(&s[2..=3], 16).unwrap())
+}
+
+fn bool_from_str<'de, D: serde::Deserializer<'de>>(d: D) -> Result<bool, D::Error> {
+    let s = <&str>::deserialize(d)?;
+    Ok(s == "1")
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 struct Header {}
@@ -7,10 +17,10 @@ struct Header {}
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 struct Event {
-    #[serde(deserialize_with = "hex_deser")]
+    #[serde(deserialize_with = "dehex")]
     event_code: u8,
 
-    #[serde(rename = "UMask", deserialize_with = "hex_deser")]
+    #[serde(rename = "UMask", deserialize_with = "dehex")]
     umask: u8,
 
     event_name: String,
@@ -18,11 +28,9 @@ struct Event {
     brief_description: String,
 
     public_description: String,
-}
 
-fn hex_deser<'de, D: serde::Deserializer<'de>>(d: D) -> Result<u8, D::Error> {
-    let s = <&str>::deserialize(d)?;
-    Ok(u8::from_str_radix(&s[2..=3], 16).unwrap())
+    #[serde(deserialize_with = "bool_from_str")]
+    any_thread: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -32,9 +40,36 @@ struct Events {
     pub events: Vec<Event>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct EventAlias {
+    name: String,
+    alias: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Metric {
+    metric_name: String,
+    legacy_name: String,
+    level: usize,
+    brief_description: String,
+    events: Vec<EventAlias>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Metrics {
+    pub header: Header,
+    pub metrics: Vec<Metric>,
+}
+
 fn main() {
     let x = include_str!("../../../extern/perfmon/SKL/events/skylake_core.json");
     let e: Events = serde_json::from_str(x).unwrap();
 
-    dbg!(e);
+    let y = include_str!("../../../extern/perfmon/SKL/metrics/skylake_metrics.json");
+    let m: Metrics = serde_json::from_str(y).unwrap();
+
+    dbg!(m);
 }
