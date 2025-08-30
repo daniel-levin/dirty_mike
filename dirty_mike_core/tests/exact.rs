f8d065c84852e7ee05101f9c67d9bbfbbd87dec2
Daniel Levin <daniellevin2607@gmail.com>
2025-08-30 13:49:31 +0200

    comparison in exact counters

--- dirty_mike_core/tests/exact.rs
+++ dirty_mike_core/tests/exact.rs
@@ -14,7 +14,10 @@ fn work(data: &[i32]) -> u64 {
 #[test]
 fn test_builder() -> anyhow::Result<()> {
     // tma_frontend_bound on SKL
-
+    // 0x13c = CPU_CLK_THREAD_UNHALTED.REF_XCLK
+    // 0x19c = IDQ_UOPS_NOT_DELIVERED.CORE
+    // 0x23c = CPU_CLK_THREAD_UNHALTED.ONE_THREAD_ACTIVE
+    // 0x3c  = CPU_CLK_UNHALTED.THREAD_P_ANY
     let exact = ExactMeasurements::builder()
         .leader(0x13c)
         .follower(0x19c)
@@ -26,11 +29,26 @@ fn test_builder() -> anyhow::Result<()> {
     let mut rng = rand::rng();
     shuffled_data.shuffle(&mut rng);
 
+    let mut sorted_data = shuffled_data.clone();
+    sorted_data.sort();
+
     let handle = exact.start().unwrap();
     work(&shuffled_data);
     let measurements = handle.stop().unwrap();
 
+    let exact2 = ExactMeasurements::builder()
+        .leader(0x13c)
+        .follower(0x19c)
+        .follower(0x23c)
+        .follower(0x3c)
+        .build()?;
+
+    let handle2 = exact2.start().unwrap();
+    work(&sorted_data);
+    let measurements2 = handle2.stop().unwrap();
+
     dbg!(measurements);
+    dbg!(measurements2);
 
     Ok(())
 }
