f8ac414d770a88833eae5a799ee2a98230263a41
Daniel Levin <daniellevin2607@gmail.com>
2025-09-02 16:19:05 +0200

    add simd example

--- dirty_mike_core/examples/a.rs
+++ dirty_mike_core/examples/a.rs
@@ -1,3 +1,8 @@
+use core::arch::x86_64::_mm_cmpeq_epi8;
+use core::arch::x86_64::_mm_loadu_epi8;
+use core::arch::x86_64::_mm_movemask_epi8;
+use core::arch::x86_64::_mm_set1_epi8;
+use core::arch::x86_64::_popcnt32;
 use dirty_mike_core::exact::*;
 
 fn main() -> anyhow::Result<()> {
@@ -6,9 +11,24 @@ fn main() -> anyhow::Result<()> {
         .measure(0x3c) // cycles
         .build()?;
 
-    let o = exact.measure(|| {})?;
+    let simd = exact.measure(|| {
+        let mut count = 0;
+        let mask = unsafe { _mm_set1_epi8('f' as i8) };
 
-    dbg!(o);
+        for i in include_str!("page.txt").as_bytes().chunks_exact(16) {
+            unsafe {
+                let chunk = _mm_loadu_epi8(i.as_ptr() as *const i8);
+                let comps = _mm_cmpeq_epi8(mask, chunk);
+                let x = _mm_movemask_epi8(comps);
+                let pop = _popcnt32(x);
+                count += pop;
+            }
+        }
+
+        count
+    })?;
+
+    dbg!(simd);
 
     Ok(())
 }
