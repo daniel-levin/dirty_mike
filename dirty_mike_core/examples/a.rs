e5e77bcbd711d0cff1ef481f4b3756c7065fbd77
Daniel Levin <daniellevin2607@gmail.com>
2025-09-02 16:38:24 +0200

    many surprises

--- dirty_mike_core/examples/a.rs
+++ dirty_mike_core/examples/a.rs
@@ -9,15 +9,18 @@ fn main() -> anyhow::Result<()> {
     let exact = ExactMeasurements::builder()
         .measure(0xc0) // ins retired
         .measure(0x3c) // cycles
+        .measure(0x10e) // uops issued
         .build()?;
 
     let simd = exact.measure(|| {
         let mut count = 0;
         let mask = unsafe { _mm_set1_epi8('f' as i8) };
 
-        for i in include_str!("page.txt").as_bytes().chunks_exact(16) {
+        let arr = include_bytes!("page.txt");
+        for off in 0..(arr.len() / 16) {
             unsafe {
-                let chunk = _mm_loadu_epi8(i.as_ptr() as *const i8);
+                let chunk =
+                    _mm_loadu_epi8(arr.as_ptr().byte_offset(16isize * (off as isize)) as *const i8);
                 let comps = _mm_cmpeq_epi8(mask, chunk);
                 let x = _mm_movemask_epi8(comps);
                 let pop = _popcnt32(x);
@@ -28,6 +31,24 @@ fn main() -> anyhow::Result<()> {
         count
     })?;
 
+    let exact = ExactMeasurements::builder()
+        .measure(0xc0) // ins retired
+        .measure(0x3c) // cycles
+        .measure(0x10e) // uops issued
+        .build()?;
+
+    let single = exact.measure(|| {
+        let mut count = 0;
+        for i in include_bytes!("page.txt") {
+            if *i == b'f' {
+                count += 1;
+            }
+        }
+
+        count
+    })?;
+
+    dbg!(single);
     dbg!(simd);
 
     Ok(())
