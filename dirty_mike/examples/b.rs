e421986039dfe3d6f3be3b4bf94742001bbf7dd8
Daniel Levin <daniellevin2607@gmail.com>
2025-09-08 15:38:37 +0200

    mispredicts

--- dirty_mike/examples/b.rs
+++ dirty_mike/examples/b.rs
@@ -4,7 +4,7 @@ use toml::Table;
 fn main() {
     let s = include_str!("../Cargo.toml");
 
-    let m = ExactMeasurements::<BadSpeculation, _>::measure_k(250, |_| {
+    let m = ExactMeasurements::<BranchMispredicts, _>::measure_k(250, |_| {
         || {
             let _f = s.parse::<Table>().unwrap();
         }
